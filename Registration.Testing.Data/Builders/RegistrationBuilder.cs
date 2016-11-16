d2b08639b9cc596e47d212ceceb2fa8112aff8e6
Marius Vorster <marius.vrstr@gmail.com>
2016-11-16 09:39:54 +0200

    Added Builder Fields

--- Registration.Testing.Data/Builders/RegistrationBuilder.cs
+++ Registration.Testing.Data/Builders/RegistrationBuilder.cs
@@ -13,18 +13,26 @@ namespace Registration.Testing.Data.Builders
 
         public RegistrationBuilder JohnDoe()
         {
+            this.Name = "John Doe";
+            this.IdentityNumber = "8504065149076";
+            this.BirthDay = new DateTime().AddYears(-31);
+            this.Subscribed = true;
 
             return this;
         }
 
         public RegistrationBuilder JaneDoe()
         {
+            this.Name = "Jane Doe";
+            this.IdentityNumber = "9002045149077";
+            this.BirthDay = new DateTime().AddYears(-29);
+            this.Subscribed = true;
 
             return this;
         }
 
 
-        public Contracts.Registration.Registration Build()
+        public Registration Build()
         {
             return this;
         }
