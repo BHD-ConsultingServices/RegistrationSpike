49b541ce00cedbe385c95529c5606f73fb6ae2fc
Marius Vorster <marius.vrstr@gmail.com>
2016-11-17 18:44:31 +0200

    Updated web controller

--- Registration.Web/Controllers/HomeController.cs
+++ Registration.Web/Controllers/HomeController.cs
@@ -1,72 +1,20 @@
-﻿using System.Web.Mvc;
-using Registration.Contracts;
-using Registration.Contracts.Registration;
-
+﻿
 namespace Registration.Web.Controllers
 {
+    using System.Web.Mvc;
+    using Models;
+    using Properties;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public JsonResult UnRegister(string identityNumber)
-        {
-            var response = new Result<Contracts.Registration.Registration>
+            var model = new BaseModel
             {
-                Data = new Contracts.Registration.Registration
-                {
-                    
-                },
-                ResultCode = ResultCode.Success
+                BaseApiUrl = Settings.Default.BaseApiUrl
             };
-            // Unregister over REST
 
-            return this.Json(response, JsonRequestBehavior.AllowGet);
+            return View(model);
         }
-
-        public JsonResult UnSubscribe(string identityNumber)
-        {
-            var response = new Result<Contracts.Registration.Registration>
-            {
-                Data = new Contracts.Registration.Registration
-                {
-
-                },
-                ResultCode = ResultCode.Success
-            };
-            // Unsubscribe over REST
-
-            return this.Json(response, JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult GetRegisteredStatus(string identityNumber)
-        {
-            var response = new Result<Status>
-            {
-                Data = Status.Registered,
-                ResultCode = ResultCode.Success
-            };
-            // Get status from REST service
-
-            return this.Json(response, JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult Register(RegistrationRequest request)
-        {
-            var response = new Result<Contracts.Registration.Registration>
-            {
-                Data = new Contracts.Registration.Registration
-                {
-
-                },
-                ResultCode = ResultCode.Success
-            };
-            // Get registration result
-
-            return this.Json(response, JsonRequestBehavior.AllowGet);
-        }
-
     }
 }
